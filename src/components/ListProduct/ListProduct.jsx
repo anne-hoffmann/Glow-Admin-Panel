b8f33b6eb08a3f1df8cc88e8201cbec9e877d9aa
Anestasia Hoffmann <anestasiahoffmann@gmail.com>
2024-01-29 16:35:01 +0200

    links updated

--- src/components/ListProduct/ListProduct.jsx
+++ src/components/ListProduct/ListProduct.jsx
@@ -6,7 +6,7 @@ const ListProduct = () => {
   const [allproducts,setAllProducts] = useState([]);
 
   const fetchInfo = async ()=>{
-    await fetch('http://localhost:4000/allproducts')
+    await fetch('https://glow-server.onrender.com/allproducts')
     .then((res)=>res.json())
     .then((data)=>{setAllProducts(data)});
   }
@@ -16,7 +16,7 @@ const ListProduct = () => {
   },[])
 
   const RemoveProduct = async (id)=>{
-    await fetch('http://localhost:4000/removeproduct',{
+    await fetch('https://glow-server.onrender.com/removeproduct',{
       method: 'POST',
       headers:{
         Accept:'application/json',
